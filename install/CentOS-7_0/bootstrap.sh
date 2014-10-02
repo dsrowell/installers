9f4e92c15dd76339410fd7b83220d71a2d9a8ec3
David Rowell <david.rowell@cd-adapco.com>
2014-10-02 13:54:15 -0400

    more updates

--- install/CentOS-7_0/bootstrap.sh
+++ install/CentOS-7_0/bootstrap.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 yum install -y httpd php bind dovecot wget gcc mariadb-server mariadb
+systemctl enable httpd.service
+systemctl enable mariadb.service
+systemctl start httpd.service
+systemctl start mariadb.service
+/usr/bin/mysql_secure_installation
