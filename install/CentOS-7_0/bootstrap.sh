587e5698e5d6e886dc8eb399b9af2e7dacb56b37
dsrowell <github@dsri.org>
2014-10-22 22:52:07 -0400

    Delete bootstrap.sh

--- install/CentOS-7_0/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-yum install -y httpd php bind dovecot wget gcc mariadb-server mariadb
-systemctl enable httpd.service
-systemctl enable mariadb.service
-systemctl start httpd.service
-systemctl start mariadb.service
-/usr/bin/mysql_secure_installation
